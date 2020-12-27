f32a458bb9199a25d962c7f88f18459b23172614
Ie Khing <iekhing@mobilewalla.com>
2020-12-27 15:49:03 +0800

    assigment5 1

--- assignments/assignment5/assignment5-solution-starter/js/script.js
+++ assignments/assignment5/assignment5-solution-starter/js/script.js
@@ -130,8 +130,10 @@ function buildAndShowHomeHTML (categories) {
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
       // ....
+      console.log (homeHtmlUrl)
       console.log (homeHtmlToInsertIntoMainPage)
-      insertHtml("#main-content", homeHtmlToInsertIntoMainPage)
+      homeHtmlToInsertIntoMainPage
+      //insertHtml("#main-content", homeHtmlToInsertIntoMainPage)
       
     },
     false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
