eb2f5ef0a1d2ed176e8d81fd05396056b239923e
Ie Khing <iekhing@mobilewalla.com>
2020-12-27 15:05:08 +0800

    assigment5 1

--- assignments/assignment5/assignment5-solution-starter/js/script.js
+++ assignments/assignment5/assignment5-solution-starter/js/script.js
@@ -122,7 +122,7 @@ function buildAndShowHomeHTML (categories) {
       var homeHtmlToInsertIntoMainPage =  
                                     insertProperty(homeHtmlUrl, 
                                     "randomCategoryShortName",  
-                                    short_name);
+                                    chosenCategoryShortName);
 
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
