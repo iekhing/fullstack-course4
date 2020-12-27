91e82715a6ffd256affc88b58c903ea24fcbccda
Ie Khing <iekhing@mobilewalla.com>
2020-12-27 15:27:14 +0800

    assigment5 1

--- assignments/assignment5/assignment5-solution-starter/js/script.js
+++ assignments/assignment5/assignment5-solution-starter/js/script.js
@@ -40,6 +40,7 @@ var showLoading = function (selector) {
 // with propValue in given 'string'
 var insertProperty = function (string, propName, propValue) {
   var propToReplace = "{{" + propName + "}}";
+  console.log("propToReplace" + propToReplace)
   string = string
     .replace(new RegExp(propToReplace, "g"), propValue);
   return string;
