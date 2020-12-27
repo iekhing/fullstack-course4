f808ce67cf3ce2adda7e95e6ad94a0363d4a6dd6
Ie Khing <iekhing@mobilewalla.com>
2020-12-27 15:31:43 +0800

    assigment5 1

--- assignments/assignment5/assignment5-solution-starter/js/script.js
+++ assignments/assignment5/assignment5-solution-starter/js/script.js
@@ -140,6 +140,7 @@ function buildAndShowHomeHTML (categories) {
 // Given array of category objects, returns a random category object.
 function chooseRandomCategory (categories) {
   // Choose a random index into the array (from 0 inclusively until array length (exclusively))
+  console.log("category " + categories)
   console.log("total category " +  categories.length)
 
   var randomArrayIndex = Math.floor(Math.random() * categories.length);
